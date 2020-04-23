607af0848a2a6abea9a3473327451a61257f1162
andrewbrodginski <abrodginski3@gatech.edu>
2020-04-23 16:31:05 -0400

    Initial commit

---
+++ Weak-Geek/src/main/java/com/workout/generator/Weak/Geek/WeakGeekApplication.java
@@ -0,0 +1,13 @@
+package com.workout.generator.Weak.Geek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeakGeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeakGeekApplication.class, args);
+	}
+
+}
---
+++ Weak-Geek/src/test/java/com/workout/generator/Weak/Geek/WeakGeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workout.generator.Weak.Geek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeakGeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
