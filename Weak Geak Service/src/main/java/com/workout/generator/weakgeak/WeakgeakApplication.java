3c329e3fba71a9c8784f369f4b7a49ad9e5a0e78
andrewbrodginski <abrodginski3@gatech.edu>
2020-04-25 10:57:43 -0400

    Adding Weak Geak Service and UI

---
+++ Weak Geak Service/src/main/java/com/workout/generator/weakgeak/WeakgeakApplication.java
@@ -0,0 +1,13 @@
+package com.workout.generator.weakgeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeakgeakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeakgeakApplication.class, args);
+    }
+
+}
