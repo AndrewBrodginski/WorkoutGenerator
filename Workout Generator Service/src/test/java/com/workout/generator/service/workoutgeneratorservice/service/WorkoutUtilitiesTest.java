68b2a39ecb5842766524c803068c0a558c4af9fe
Brodginski, Andrew <brodginskiar@upmc.edu>
2021-01-29 15:14:27 -0500

    Trying to figure out logging

--- Workout Generator Service/src/test/java/com/workout/generator/service/workoutgeneratorservice/service/WorkoutUtilitiesTest.java
+++ Workout Generator Service/src/test/java/com/workout/generator/service/workoutgeneratorservice/service/WorkoutUtilitiesTest.java
@@ -2,6 +2,7 @@ package com.workout.generator.service.workoutgeneratorservice.service;
 
 
 
+import com.workout.generator.service.workoutgeneratorservice.data.WorkoutType;
 import org.junit.Before;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -9,18 +10,26 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@Slf4j
 public class WorkoutUtilitiesTest {
 
-    private WorkoutUtilities workoutUtilities = new WorkoutUtilities();
+    private WorkoutUtilities workoutUtilities;
 
     @Before
     public void setUp() {
-
+        workoutUtilities = new WorkoutUtilities();
     }
 
     @Test
     public void getRandomNumber_ZeroProvided_IntNotReturned(){
         int randomInt = workoutUtilities.getRandomNumber(0);
         assertNotNull(randomInt);
+    }
+
+    @Test
+    public void getRandomWorkoutType_IsCalled_WorkoutTypeReturned(){
+        WorkoutType workoutType = workoutUtilities.getRandomWorkoutType();
+
+        assertNotNull(workoutType);
     }
 }
